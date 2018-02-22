efba44728325a39d4613c7c4b97490596ba5b7b4
ravi <rrk736@gmail.com>
2018-02-22 18:01:19 +0530

    Changes

---
+++ ProjectForJenkins/src/com/java/emids/jenkins/SampleForJenkins.java
@@ -0,0 +1,11 @@
+package com.java.emids.jenkins;
+
+public class SampleForJenkins {
+
+	public static void main(String[] args) {
+		System.out.println("*********HELLO JENKINS*********");
+		System.out.println("*********Another HELLO JENKINS*********");
+
+	}
+
+}
